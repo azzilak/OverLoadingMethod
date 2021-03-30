ccae3c3d5eba11c2cfd09e67e931c29ea82c49c6
azzilak <azzilakaren@students.amikom.ac.id>
2021-03-30 15:31:33 +0700

    Add project files.

---
+++ OverLoadingMethod/Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum (int number1, int number2)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public int FindMinimum (params int [] values)
+        {
+            return Enumerable.Min(values);
+        }
+
+        public int FindMaximum (int number1, int number2)
+        {
+            return Math.Max(number1, number2);
+        }
+
+        public int FindMaximum (params int [] values)
+        {
+            return Enumerable.Max(values);
+        }
+    }
+}
